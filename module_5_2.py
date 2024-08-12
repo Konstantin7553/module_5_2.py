66ff8ac12880caf9e787bd5772202ac73643069c
Konstantin7553 <krasik150@gmail.com>
2024-08-12 11:48:18 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,17 @@
+from module_5_1 import House
+
+my_house = House("ЖК Эльбрус", 30)
+my_house1 = House("Домик в деревне", 3)
+
+print(my_house)
+print(my_house1)
+
+print(len(my_house))
+print(len(my_house1))
+
+
+
+
+
+
+
